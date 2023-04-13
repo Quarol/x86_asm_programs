e5e1d834996f8e1b9b4053da4a22bac9dff0b7cd
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2023-04-13 16:45:31 +0200

    Add files via upload
    Programs performing given operation on fixed-size numbers - larger than a word.
    The programs divide data into 32-bit (32-bit word) numbers, perfoms a given operation on them and push every partial sum on the stack.
    To view the content of stack - use "x/9xw $sp" command in GDB debbugger.

---
+++ big_numbers_computing/subtraction.s
@@ -0,0 +1,62 @@
+.code32
+# x/9xw $sp - viewing 9 elements from stack content
+# info registers <register name> - viewing register(s)
+.section .data
+liczba1:
+    .long 0x10304008, 0x701100FF, 0x45100020, 0x08570030
+	
+liczba2:
+	.long 0xF040500C, 0x00220026, 0x321000CB, 0x04520031
+
+# the 2nd complement result(s) in hex:
+#		  0x1FEFEFFC, 0x6FEF00D9, 0x12FFFF55, 0x0404FFFF
+#		  Unf = 	, Unf = 	, Unf =    , Unf = 
+# the natural binary result(s) in hex:
+#		  0xE0101003, 0x9010FF26, 0xED0000AA, 0xFBFB0001
+#		  Unf = 	, Unf = 	, Unf =    , Unf = 
+# Unf - underflow
+
+# both liczba1 and liczba2 have the same number of bytes
+# that number equals 16 in total
+# that means 4 bytes per fragment
+
+
+.section .text
+.global _start
+SYSEXIT = 1
+EXIT_SUCCESS = 0
+SYSCALL = 0x80
+
+_start:
+	# esi used as index
+	movl $4, %esi
+	# clearing needed registers
+	xor %eax, %eax
+	xor %ebx, %ebx	# setting carry to 0
+	xor %ecx, %ecx
+	xor %edx, %edx
+	clc
+
+	_loop: # in range[3, 0]:
+		subl $1, %esi
+		
+		movl liczba1(, %esi, 4), %eax
+		subl liczba2(, %esi, 4), %eax
+		setc %cl	# carry from adding two parts
+		subl %ebx, %eax
+		setc %bl	# carry from adding previous carry
+		or %cl, %bl	# checking if there's carry
+		pushl %eax
+
+		cmp $0, %esi
+		jne _loop
+
+	# we also have to push last carry
+	pushl %ebx
+	result: # breakpoint to watch stack content
+
+	# THE END
+	movl $SYSEXIT, %eax
+	movl $EXIT_SUCCESS, %ebx
+	int $SYSCALL
+   
