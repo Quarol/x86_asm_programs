e551b128c56e15277fb9375c6237851b37fe644d
Quarol <karol.charchut@gmail.com>
2023-05-13 15:01:18 +0200

    SISD functions added and the results are put in the "result" file

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -64,12 +64,12 @@ void div_SIMD(Vector v1, Vector v2)
 
     asm(
         "movups %[v1], %%xmm0;"
-        "divps %[v2], %%xmm1;"
+        "movups %[v2], %%xmm1;"
         "divps %%xmm1, %%xmm0;"
         "movups %%xmm0, %[res];"
         : [res] "=rm" (res)
         : [v1] "irm" (v1), [v2] "irm" (v2)
-    ); 
+    );
 }
 
 
@@ -78,19 +78,36 @@ void add_SISD(Vector v1, Vector v2)
     Vector res;
 
     asm (
-        ""
-        : [x3] "=rm" (res.x3),
-        [x2] "=rm" (res.x2),
-        [x1] "=rm" (res.x2),
-        [x0] "=rm" (res.x0)
-        : [a3] "irm" (v1.x3),
-        [a2] "irm" (v1.x2),
-        [a1] "irm" (v1.x1),
-        [a0] "irm" (v1.x0),
-        [b3] "irm" (v2.x3),
-        [b2] "irm" (v2.x2),
-        [b1] "irm" (v2.x1),
-        [b0] "irm" (v2.x0)
+            "fld %[a0];"
+            "fadd %[b0];"
+            "fstp %[x0];"
+
+            "fld %[a1];"
+            "fadd %[b1];"
+            "fstp %[x1];"
+
+            "fld %[a2];"
+            "fadd %[b2];"
+            "fstp %[x2];"
+
+            "fld %[a3];"
+            "fadd %[b3];"
+            "fstp %[x3];"
+
+            : [x3] "=m" (res.x3),
+            [x2] "=m" (res.x2),
+            [x1] "=m" (res.x1),
+            [x0] "=m" (res.x0)
+
+            : [a3] "m" (v1.x3),
+            [a2] "m" (v1.x2),
+            [a1] "m" (v1.x1),
+            [a0] "m" (v1.x0),
+
+            [b3] "m" (v2.x3),
+            [b2] "m" (v2.x2),
+            [b1] "m" (v2.x1),
+            [b0] "m" (v2.x0)
     );
 }
 
@@ -100,19 +117,36 @@ void sub_SISD(Vector v1, Vector v2)
     Vector res;
 
     asm (
-        ""
-        : [x3] "=rm" (res.x3),
-        [x2] "=rm" (res.x2),
-        [x1] "=rm" (res.x2),
-        [x0] "=rm" (res.x0)
-        : [a3] "irm" (v1.x3),
-        [a2] "irm" (v1.x2),
-        [a1] "irm" (v1.x1),
-        [a0] "irm" (v1.x0),
-        [b3] "irm" (v2.x3),
-        [b2] "irm" (v2.x2),
-        [b1] "irm" (v2.x1),
-        [b0] "irm" (v2.x0)
+            "fld %[a0];"
+            "fsub %[b0];"
+            "fstp %[x0];"
+
+            "fld %[a1];"
+            "fsub %[b1];"
+            "fstp %[x1];"
+
+            "fld %[a2];"
+            "fsub %[b2];"
+            "fstp %[x2];"
+
+            "fld %[a3];"
+            "fsub %[b3];"
+            "fstp %[x3];"
+
+            : [x3] "=m" (res.x3),
+            [x2] "=m" (res.x2),
+            [x1] "=m" (res.x1),
+            [x0] "=m" (res.x0)
+
+            : [a3] "m" (v1.x3),
+            [a2] "m" (v1.x2),
+            [a1] "m" (v1.x1),
+            [a0] "m" (v1.x0),
+
+            [b3] "m" (v2.x3),
+            [b2] "m" (v2.x2),
+            [b1] "m" (v2.x1),
+            [b0] "m" (v2.x0)
     );
 }
 
@@ -122,19 +156,36 @@ void mul_SISD(Vector v1, Vector v2)
     Vector res;
 
     asm (
-        ""
-        : [x3] "=rm" (res.x3),
-        [x2] "=rm" (res.x2),
-        [x1] "=rm" (res.x2),
-        [x0] "=rm" (res.x0)
-        : [a3] "irm" (v1.x3),
-        [a2] "irm" (v1.x2),
-        [a1] "irm" (v1.x1),
-        [a0] "irm" (v1.x0),
-        [b3] "irm" (v2.x3),
-        [b2] "irm" (v2.x2),
-        [b1] "irm" (v2.x1),
-        [b0] "irm" (v2.x0)
+            "fld %[a0];"
+            "fmul %[b0];"
+            "fstp %[x0];"
+
+            "fld %[a1];"
+            "fmul %[b1];"
+            "fstp %[x1];"
+
+            "fld %[a2];"
+            "fmul %[b2];"
+            "fstp %[x2];"
+
+            "fld %[a3];"
+            "fmul %[b3];"
+            "fstp %[x3];"
+
+            : [x3] "=m" (res.x3),
+            [x2] "=m" (res.x2),
+            [x1] "=m" (res.x1),
+            [x0] "=m" (res.x0)
+
+            : [a3] "m" (v1.x3),
+            [a2] "m" (v1.x2),
+            [a1] "m" (v1.x1),
+            [a0] "m" (v1.x0),
+
+            [b3] "m" (v2.x3),
+            [b2] "m" (v2.x2),
+            [b1] "m" (v2.x1),
+            [b0] "m" (v2.x0)
     );
 }
 
@@ -144,19 +195,36 @@ void div_SISD(Vector v1, Vector v2)
     Vector res;
 
     asm (
-        ""
-        : [x3] "=rm" (res.x3),
-        [x2] "=rm" (res.x2),
-        [x1] "=rm" (res.x2),
-        [x0] "=rm" (res.x0)
-        : [a3] "irm" (v1.x3),
-        [a2] "irm" (v1.x2),
-        [a1] "irm" (v1.x1),
-        [a0] "irm" (v1.x0),
-        [b3] "irm" (v2.x3),
-        [b2] "irm" (v2.x2),
-        [b1] "irm" (v2.x1),
-        [b0] "irm" (v2.x0)
+            "fld %[a0];"
+            "fdiv %[b0];"
+            "fstp %[x0];"
+
+            "fld %[a1];"
+            "fdiv %[b1];"
+            "fstp %[x1];"
+
+            "fld %[a2];"
+            "fdiv %[b2];"
+            "fstp %[x2];"
+
+            "fld %[a3];"
+            "fdiv %[b3];"
+            "fstp %[x3];"
+
+            : [x3] "=m" (res.x3),
+            [x2] "=m" (res.x2),
+            [x1] "=m" (res.x1),
+            [x0] "=m" (res.x0)
+
+            : [a3] "m" (v1.x3),
+            [a2] "m" (v1.x2),
+            [a1] "m" (v1.x1),
+            [a0] "m" (v1.x0),
+
+            [b3] "m" (v2.x3),
+            [b2] "m" (v2.x2),
+            [b1] "m" (v2.x1),
+            [b0] "m" (v2.x0)
     );
 }
 
@@ -211,7 +279,7 @@ double get_average_time(function f, Vector numbers1[], Vector numbers2[], int nu
 std::string generate_output(int num_of_elements1, int num_of_elements2, int num_of_elements3,
 				 int repetitions, Vector v1[], Vector v2[])
 {
-    float add, sub, mul, div;
+    float addSimd, subSimd, mulSimd, divSimd;
     int elements[3] = {num_of_elements3, num_of_elements2, num_of_elements1};
     int nums_in_array;
 
@@ -220,41 +288,45 @@ std::string generate_output(int num_of_elements1, int num_of_elements2, int num_
     {
 	nums_in_array = elements[i] / 2 / 4; 
 
-    	add = get_average_time(add_SIMD, v1, v2, nums_in_array, repetitions);
-    	sub = get_average_time(sub_SIMD, v1, v2, nums_in_array, repetitions);
-    	mul = get_average_time(mul_SIMD, v1, v2, nums_in_array, repetitions);
-    	div = get_average_time(div_SIMD, v1, v2, nums_in_array, repetitions);
+    	addSimd = get_average_time(add_SIMD, v1, v2, nums_in_array, repetitions);
+    	subSimd = get_average_time(sub_SIMD, v1, v2, nums_in_array, repetitions);
+    	mulSimd = get_average_time(mul_SIMD, v1, v2, nums_in_array, repetitions);
+    	divSimd = get_average_time(div_SIMD, v1, v2, nums_in_array, repetitions);
 
     	output += "Typ obliczen: SIMD\n";
     	output += "Liczba liczb: " + std::to_string(elements[i]) + "\n";
     	output += "Sredni czas [nanosekundy]\n";
-    	output += "+ " + std::to_string(add) + "\n";
-    	output += "- " + std::to_string(sub) + "\n";
-    	output += "* " + std::to_string(mul) + "\n";
-    	output += "/ " + std::to_string(div);
+    	output += "+ " + std::to_string(addSimd) + "\n";
+    	output += "- " + std::to_string(subSimd) + "\n";
+    	output += "* " + std::to_string(mulSimd) + "\n";
+    	output += "/ " + std::to_string(divSimd);
 
 	if (i != 2)
 	    output += "\n\n";
     }
     output += "\n---------------------------------------------\n\n\n";
 
+
+    float addSisd, subSisd, mulSisd, divSisd;
+
+
     output += "---------------------------------------------\n";
     for (int i = 0; i < 3; ++i)
     {
         nums_in_array = elements[i] / 2 / 4;
 
-    	add = get_average_time(add_SISD, v1, v2, nums_in_array, repetitions);
-    	sub = get_average_time(sub_SISD, v1, v2, nums_in_array, repetitions);
-    	mul = get_average_time(mul_SISD, v1, v2, nums_in_array, repetitions);
-    	div = get_average_time(div_SISD, v1, v2, nums_in_array, repetitions);
+        addSisd = get_average_time(add_SISD, v1, v2, nums_in_array, repetitions);
+        subSisd = get_average_time(sub_SISD, v1, v2, nums_in_array, repetitions);
+        mulSisd = get_average_time(mul_SISD, v1, v2, nums_in_array, repetitions);
+        divSisd = get_average_time(div_SISD, v1, v2, nums_in_array, repetitions);
 
     	output += "Typ obliczen: SISD\n";
     	output += "Liczba liczb: " + std::to_string(elements[i]) + "\n";
     	output += "Sredni czas [nanosekundy]\n";
-    	output += "+ " + std::to_string(add) + "\n";
-    	output += "- " + std::to_string(sub) + "\n";
-    	output += "* " + std::to_string(mul) + "\n";
-    	output += "/ " + std::to_string(div);
+    	output += "+ " + std::to_string(addSisd) + "\n";
+    	output += "- " + std::to_string(subSisd) + "\n";
+    	output += "* " + std::to_string(mulSisd) + "\n";
+    	output += "/ " + std::to_string(divSisd);
 
 	if (i != 2)
 	    output += "\n\n";
@@ -309,10 +381,6 @@ int main()
     Vector v1[nums_in_array];
     Vector v2[nums_in_array];
     random_fill_arrays(v1, v2, nums_in_array, lower_bound, upper_bound);
-    
-    // "heating up" CPU before saving
-    generate_output(num_of_elements1, num_of_elements2, num_of_elements3,
-				repetitions, v1, v2);
 
     std::string output = generate_output(num_of_elements1, num_of_elements2, num_of_elements3,
 				repetitions, v1, v2);
@@ -321,6 +389,6 @@ int main()
     file.open("results.txt");
     file << output;
     file.close();
-    
+
     return 0;
 }
